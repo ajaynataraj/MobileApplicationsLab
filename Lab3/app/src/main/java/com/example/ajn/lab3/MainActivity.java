381d488188d73d8144a96412f8f302b1e455efe8
ajaynataraj <ajaynataraj26121997@gmail.com>
2018-01-30 00:55:22 +0530

    Implement Timer of 3 seconds

--- Lab3/app/src/main/java/com/example/ajn/lab3/MainActivity.java
+++ Lab3/app/src/main/java/com/example/ajn/lab3/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ajn.lab3;
 
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -19,62 +22,67 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.TextView;
 
+import static com.example.ajn.lab3.MainActivity.PlaceholderFragment.ARG_SECTION_NUMBER;
+
 public class MainActivity extends AppCompatActivity {
+    Handler handler = new Handler();
+    Runnable runnable;
+    @Override
+    protected void onDestroy() {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
+        handler.removeCallbacks(runnable);
+        super.onDestroy();
+    }
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
+    static int a=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        /*new CountDownTimer(3000, 1000) {
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            public void onFinish() {
+                PlaceholderFragment fragment = new PlaceholderFragment();
+                Bundle args = new Bundle();
+                a++;
+                args.putInt(ARG_SECTION_NUMBER, (a%4)+1);
+                fragment.setArguments(args);
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.fragment_container, fragment);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.commit();
+            }
+        }.start();*/
+
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                PlaceholderFragment fragment = new PlaceholderFragment();
+                Bundle args = new Bundle();
+                a++;
+                args.putInt(ARG_SECTION_NUMBER, (a%4)+1);
+                fragment.setArguments(args);
+                //Second fragment after 5 seconds appears
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container,fragment )
+                        .commit();
+                handler.postDelayed(this, 3000);
+            }
+        };
+
+        handler.postDelayed(runnable, 3000);
 
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     /**
      * A placeholder fragment containing a simple view.
@@ -85,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
          * fragment.
          */
         static int frag_no;
-        private static final String ARG_SECTION_NUMBER = "section_number";
+        public static final String ARG_SECTION_NUMBER = "section_number";
 
         public PlaceholderFragment() {
         }
@@ -117,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
             } else if(getArguments().getInt(ARG_SECTION_NUMBER)==3){
                 rootView = inflater.inflate(R.layout.fragment_three, container, false);
                 WebView myWebView =  rootView.findViewById(R.id.webview);
-                //myWebView.loadUrl("http://www.nitt.edu");
+                //myWebView.loadUrl("https://www.duckduckgo.com");
             } else {
                 rootView = inflater.inflate(R.layout.fragment_four, container, false);
             }
@@ -126,27 +134,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 4;
-        }
-    }
 }
